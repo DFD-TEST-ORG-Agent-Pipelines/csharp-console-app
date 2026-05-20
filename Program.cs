807f4d94d51fddcb33dd9801f3e96ce37959a112
Michael Sterenberg <michast@microsoft.com>
2026-05-20 11:06:30 +0300

    Add sample application code and CI/CD pipelines

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name:");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Hello, {name}!");
+            
+            int result = Calculate(10, 5);
+            Console.WriteLine($"10 + 5 = {result}");
+        }
+        
+        static int Calculate(int a, int b) => a + b;
+    }
+}
